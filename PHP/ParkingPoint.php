e0c30d6a4904220e4f6e43357ec492f6c2f0b299
AnDeoukKyi <44719109+AnDeoukKyi@users.noreply.github.com>
2019-03-24 16:21:19 +0900

    PARKINGPOINT.php

---
+++ PHP/ParkingPoint.php
@@ -0,0 +1,14 @@
+﻿<?php
+    header("Connect-Type: text/html; charset=UTF-8");
+    $con = mysqli_connect("localhost", "nejoo97", "njeunjoo1997!", "nejoo97");
+
+
+    $result = mysqli_query($con, "SELECT PARKINGPOINT.POINT, PARKINGPOINT.OCCUPY, PARKINGPOINT.STARTX, PARKINGPOINT.STARTY, PARKINGPOINT.LENX, PARKINGPOINT.LENY FROM PARKINGPOINT");
+    $response = array();
+    while($row = mysqli_fetch_array($result)){
+        array_push($response, array("POINT"=>$row[0], "OCCUPY"=>$row[1], "STARTX"=>$row[2], "STARTY"=>$row[3], "LENX"=>$row[4], "LENY"=>$row[5]));
+    }
+
+    echo json_encode(array("response"=>$response), JSON_UNESCAPED_UNICODE);
+    mysqli_close($con);
+?>
